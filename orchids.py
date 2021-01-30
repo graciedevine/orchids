c783fb1cbb266d4ce574959d9986a9e05c4c53c5
Gracie <68759643+graciedevine@users.noreply.github.com>
2021-01-30 17:13:10 -0500

    Add files via upload

--- orchids.py
+++ orchids.py
@@ -1,96 +1,78 @@
-# Orchid watering app!
-
 import json
 from datetime import date
 
-#Program to print orchids and latest watering dates
-# with open("orchids.json") as f:
-#     data = json.load(f)
 
 def load():
+    """Opens JSON file as Python object."""
     with open('orchids.json') as f:
         orchid_array = json.load(f)
         return orchid_array
 
+
 orchid_data = load()
 
-for k in orchid_data["orchid_details"]:
-    x = len(k["dates"])
-    print(k["name"], " was last watered on ", k["dates"][x-1])
 
-while True: #Program loop
-     
-    def rewrite(data, filename="orchids.json"):
-        """Rewrites data to JSON array""" 
-        with open(filename,'w') as f: 
-            json.dump(data, f, indent = 4)
+def rewrite(orchid_data, filename="orchids.json"):
+    """Rewrites data to JSON array"""
+    with open(filename, 'w') as f:
+        json.dump(orchid_data, f, indent=4)
 
-    def add_orchid():
-        """Takes user input, appends orchid name to array"""
-        new_orchid = input("Enter name of orchid that you would like to add\n")
 
-        temp = orchid_data["orchid_details"]
+def add_orchid():
+    """Adds a new entry to array."""
+    new_orchid = input("Who would you like to add?\n")
 
-        y = {"name": new_orchid,
-            "dates": []
-            } # Python object to be appended
+    d = {"name": new_orchid,
+         "dates": []
+         }  # Python object to be appended.
 
-        temp.append(y)
-        
-        rewrite(orchid_data)
-        print(new_orchid + " has been added!")
-        
-    def add_watering():
-        """Appends datetime to 'names' """
-        water_event = input("Which plant got watered?\n")
+    orchid_data.append(d)
+    rewrite(orchid_data)
+    print(new_orchid + " has been added!")
 
-        with open("orchids.json") as json_file:
-            data = json.load(json_file)
 
-            temp = data["orchid_details"]
+def add_watering():
+    """Appends watering event to orchid."""
+    water_event = input("Which plant got watered?\n")
 
-            for key in temp:
-                if (water_event == key["name"]): 
-                    today = date.today()
-                    key["dates"].append(today.strftime("%m/%d/%Y"))  #change current date to string
+    for d in orchid_data:
+        if water_event == d["name"]:
+            today = date.today()
+            d["dates"].append(today.strftime("%m/%d/%Y"))  # change current date to string.
+    rewrite(orchid_data)
+    print(water_event + " has been updated!")
 
-            rewrite(data)
-            print(water_event + " has been updated!")
-            return
 
-    def remove_orchid():
-        """Deletes a key value pair from array"""
-        delete_orchid = input("Which orchid do you want to delete?\n")
-        with open("orchids.json") as json_file:
-            data = json.load(json_file)
+def remove_orchid():
+    """Deletes an entry from array."""
+    delete_orchid = input("Which orchid do you want to delete?\n")
 
-            temp = data["orchid_details"]
+    for d in orchid_data:
+        if delete_orchid == d["name"]:
+            orchid_data.remove(d)
+    rewrite(orchid_data)
+    print(delete_orchid + " has been removed.")
 
-            for key in temp:
-                if (delete_orchid == key["name"]):
-                    temp.remove(key)
 
-            rewrite(data)
-            print(delete_orchid + " has been removed.")
-            return
+def watering_history():
+    """Prints all of an orchid's watering dates."""
+    who_water = input("Whose watering history do you want to see?\n")
 
-    def watering_history():
-        """Takes user input, prints orchid's watering history"""
-        who_water = input("Whose watering history do you want to see?\n")
+    for x in orchid_data:
+        if who_water == x["name"]:
+            for y in x["dates"]:
+                print(y)
 
-        with open('orchids.json') as f:
-            data = json.load(f)
 
-            temp = data["orchid_details"]
+# Print the latest watering dates of orchids.
+for d in orchid_data:
+    x = len(d["dates"])
+    print(d["name"], " was last watered on ", d["dates"][-1])
 
-            for x in temp:
-                if(who_water == x["name"]):
-                    for y in x["dates"]:
-                        print(y)
-        return
-
-    #Menu options
-    anything_else = input("\nWould you like to...?\n1. Add an orchid\n2. Add a watering\n3. Remove an orchid\n4. View an orchid's watering history\n")
+while True:  # Menu options loop.
+    anything_else = input(
+        "\nWould you like to...?\n1. Add an orchid\n2. Add a watering\n"
+        "3. Remove an orchid\n4. View an orchid's watering history\n")
 
     if anything_else == "1":
         add_orchid()
@@ -107,7 +89,7 @@ while True: #Program loop
     else:
         print("Enter a number 1-4.")
 
-    #Ask if user wants to do anything else, otherwise exit program
-    go_again = input("Would you like to do anything else?\n")
-    if go_again.lower() != "yes":
+    # Ask if user wants to do anything else, otherwise exit program.
+    go_again = input("Would you like to do anything else? (y/n)\n")
+    if go_again.lower() != "y":
         break
